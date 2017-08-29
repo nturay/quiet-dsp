357aba0ffaf5a1ace3d93d293b5ddff8bbc2f19a
Brian Armstrong <brian.armstrong.ece+github@gmail.com>
2017-08-29 00:32:19 -0700

    is sinf somehow actually present

--- src/utility/src/single_fp.c
+++ src/utility/src/single_fp.c
@@ -28,10 +28,4 @@
 #include "liquid.internal.h"
 
 #ifndef HAVE_SINGLE_FP
-extern "C" {
-float sinf(float f) { return (float)sin(f); }
-float cosf(float f) { return (float)cos(f); }
-float expf(float f) { return (float)exp(f); }
-float sqrtf(float f) { return (float)sqrt(f); }
-}
 #endif
